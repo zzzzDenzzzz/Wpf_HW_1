c082826a0a0fe92c795d4b53fb4030bf842e4185
12den24@gmail.com <12den24@gmail.com>
2022-11-22 20:22:37 +0300

    Добавьте файлы проекта.

---
+++ Task_1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Resize(object sender, SizeChangedEventArgs e)
+        {
+            wrapPanel.Width = 2.0 / 3.0 * ActualWidth;
+            Title = $"Width wrapPanel: {Math.Round(wrapPanel.Width, 2)}";
+        }
+    }
+}
